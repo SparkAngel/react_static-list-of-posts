184e503b18348724d8b7dd5ade3f99b4502a5dae
SparkAngel <igor.gnatenko555@gmail.com>
2019-07-03 19:40:07 +0300

    Updates

---
+++ precache-manifest.16d104daddcd00f4d77392639e84206c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20609f375583e0f502b32ac45c2cf23",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "13968029b903e8b81029",
+    "url": "/react_static-list-of-posts/static/css/main.6d11e364.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react_static-list-of-posts/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "13968029b903e8b81029",
+    "url": "/react_static-list-of-posts/static/js/main.5f0a69ae.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
